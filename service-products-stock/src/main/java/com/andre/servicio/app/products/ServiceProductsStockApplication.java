74c5986f13eb47eb31cec3aaef80f46b2af55277
andrepalacios <andre.palacios@gmail.com>
2020-03-23 18:33:03 -0600

    eureka server and zuul gateway initial

--- service-products-stock/src/main/java/com/andre/servicio/app/products/ServiceProductsStockApplication.java
+++ service-products-stock/src/main/java/com/andre/servicio/app/products/ServiceProductsStockApplication.java
@@ -2,7 +2,9 @@ package com.andre.servicio.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceProductsStockApplication {
 
