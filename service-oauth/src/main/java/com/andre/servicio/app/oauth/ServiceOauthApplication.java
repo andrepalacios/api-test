4d58edf58e66230dd6f6a3ab32d76e7a8d195fec
andrepalacios <andre.palacios@gmail.com>
2020-04-09 16:27:35 -0600

    service oauth initial

---
+++ service-oauth/src/main/java/com/andre/servicio/app/oauth/ServiceOauthApplication.java
@@ -0,0 +1,17 @@
+package com.andre.servicio.app.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOauthApplication.class, args);
+	}
+
+}
