b6db29bedc04140fe348536b5aaf73f20dad3d3f
andrepalacios <andre.palacios@gmail.com>
2020-04-09 19:28:25 -0600

    adding security

--- service-users/src/main/resources/import.sql
+++ service-users/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('andres', '12345', 1, 'Andres', 'Guzman', 'guz@mail.com');
-INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'John', 'Doe', 'jhon@mail.com');
+INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('jose', '$2a$10$eOfZx.rw3bYIWE6AEPDYiOrz1lf6U67uq0E4FKh5XrqDzIBLDX6pq', 1, 'Andres', 'Guzman', 'guz@mail.com');
+INSERT INTO `users` (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$IRfwRfHOZiBLz.dNu.2v8erW4HGEVtO1w7EhqSiict6jKjbqIXYU6', 1, 'John', 'Doe', 'jhon@mail.com');
 
 INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
